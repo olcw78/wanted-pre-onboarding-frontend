6bf47884b0b548f66f548eb97c13bb918378449e
yoonsang lee <highp0912@pm.me>
2023-04-19 03:08:05 +0900

    todo 추가 부분까지

--- src/feature/todos/TodoItem.tsx
+++ src/feature/todos/TodoItem.tsx
@@ -5,27 +5,37 @@ import type { TodoModel } from "../../network/spec/todo/model/TodoModel";
 export interface TodoItemProps
   extends Pick<TodoModel, "todo" | "isCompleted">,
     Pick<HTMLAttributes<HTMLElement>, "className"> {
-  order: number;
+  id: number;
   onToggle: (id: number) => void;
 }
 
 const TodoItem: FC<TodoItemProps> = ({
-  order,
+  id,
   todo,
   isCompleted,
   onToggle,
   className
 }) => (
-  <li className={"bg-amber-100 rounded-md px-3 py-2 ".concat(className || "")}>
-    <label>
+  <li className={"bg-pink-200 rounded-md px-3 py-2 ".concat(className || "")}>
+    <label className="mr-10">
       <input
         type="checkbox"
         checked={isCompleted}
-        onChange={() => onToggle(order)}
+        onChange={() => onToggle(id)}
         className="mr-4"
       />
-      <span className="text-2xl text-bold">{todo}</span>
+      <span className="text-2xl text-bold text-slate-700 w-[250px]">
+        {todo}
+      </span>
     </label>
+
+    <button className="bg-violet-300 rounded-md px-3 py-1 mr-5">
+      <span className="text-white text-xl">수정</span>
+    </button>
+
+    <button className="bg-red-300 rounded-md px-3 py-1">
+      <span className="text-white text-xl">삭제</span>
+    </button>
   </li>
 );
 export default TodoItem;
--- src/pages/todos.page.tsx
+++ src/pages/todos.page.tsx
@@ -1,5 +1,5 @@
-import type { FC } from "react";
-import { useState } from "react";
+import type { ChangeEventHandler, FC } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { TodoModel } from "../network/spec/todo/model/TodoModel";
 import TodoItem from "../feature/todos/TodoItem";
 
@@ -25,29 +25,92 @@ const TodosPage: FC = () => {
     }
   ]);
 
-  const toggleHandler = (order: number) => {
+  const newTodoInputRef = useRef<HTMLInputElement>(null);
+  const [newTodoInput, setNewTodoInput] = useState("");
+
+  useEffect(() => {
+    newTodoInputRef?.current?.focus();
+  }, [newTodoInputRef]);
+
+  const updateNewTodoInput: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setNewTodoInput(event.target.value);
+  };
+
+  const toggleHandler = (id: number) => {
+    const foundIdx = todos.findIndex((todo) => todo.id === id);
+    if (foundIdx === -1) {
+      return;
+    }
+
     setTodos([
-      ...todos.slice(0, order),
+      ...todos.slice(0, foundIdx),
       {
-        ...todos[order],
-        isCompleted: !todos[order].isCompleted
+        ...todos[foundIdx],
+        isCompleted: !todos[foundIdx].isCompleted
       },
-      ...todos.slice(order + 1)
+      ...todos.slice(foundIdx + 1)
     ]);
   };
 
+  const addNewTodoHandler = () => {
+    const newTodo = {
+      id: todos.length + 1,
+      todo: newTodoInput,
+      isCompleted: false,
+      userId: 1
+    };
+
+    setTodos([...todos, newTodo]);
+    setNewTodoInput("");
+
+    newTodoInputRef?.current?.focus();
+  };
+
+  const editTodo = (id: number) => {
+    const foundIdx = todos.findIndex((todo) => todo.id === id);
+    if (foundIdx === -1) {
+      return;
+    }
+  };
+
+  console.log(todos);
+
   return (
-    <ul>
-      {todos?.map((todo, i) => (
-        <TodoItem
-          key={todo.id}
-          order={i}
-          onToggle={toggleHandler}
-          className="my-2"
-          {...todo}
+    <div>
+      <section className="mb-5 bg-slate-200 px-5 py-3 rounded-md mx-auto">
+        {/* 새 Todo 항목 인풋 필드 */}
+        <input
+          type="text"
+          data-testid="new-todo-input"
+          className="border border-1 border-slate-300 rounded-md mr-3"
+          value={newTodoInput}
+          onChange={updateNewTodoInput}
+          ref={newTodoInputRef}
         />
-      ))}
-    </ul>
+
+        {/* 새 Todo 항목 추가 버튼 */}
+        <button
+          type="button"
+          data-testid="new-add-todo-button"
+          className="bg-slate-600 px-3 py-1 rounded-md"
+          onClick={addNewTodoHandler}
+        >
+          <span className="text-white text-bold text-xl">추가</span>
+        </button>
+      </section>
+
+      {/* 현재 Todo 항목들 iterate & render */}
+      <ul>
+        {todos?.map((todo, i) => (
+          <TodoItem
+            key={todo.id}
+            onToggle={toggleHandler}
+            className="my-2"
+            {...todo}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
